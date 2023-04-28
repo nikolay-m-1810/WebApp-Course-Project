38fd465af7ed8f67bb9baa356dac394b4e3d45c2
Rado236 <55925335+Rado236@users.noreply.github.com>
2023-04-28 20:25:04 +0300

    registration working

--- frontend/angular/crypto/src/app/registration/registration.component.ts
+++ frontend/angular/crypto/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+    username:string ='';
+    password:string = '';
+    email:string = '';
 
+  constructor(private http: HttpClient) {  
+  }
+
+  onSubmit() {
+    // send login data to backend
+    this.http.post('http://localhost:8080/api/user/register',{username:this.username, password:this.password,email:this.email},)
+      .subscribe(() => {
+        console.log("registered succsefully!!");
+      }, error => {
+        // login failed
+        console.error(error);
+      });
+
+  }
 }
