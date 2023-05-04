752d1545de2725c2555045218e9c95b399c6ce1b
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-05-04 22:33:17 +0300

    first deposit

--- models/priceModell.ts
+++ models/priceModell.ts
@@ -14,4 +14,27 @@ export class PriceModell {
         const [rows] = await this.conn.query("SELECT * FROM prices");
         return rows;
     }
+    async deposit(pub: string, dep: number) {
+        const conn = await this.conn.getConnection();
+        try {
+            await conn.beginTransaction(); // start transaction
+
+            const [usdt] = await conn.query("SELECT amount FROM wallet_contents WHERE crypto_id=? AND public_address=?", [4, pub]);
+            console.log(`The inserted price is ${dep}`);
+
+            if (usdt.length != 0) { //already have usdt in the account
+                await conn.execute("UPDATE wallet_contents SET amount = amount+? WHERE public_address=? AND crypto_id=?", [dep, pub, 4]);
+            } else {
+                await conn.execute("INSERT INTO wallet_contents (public_address,crypto_id,amount) VALUES (?, ?, ?)", [pub, 4, dep]);
+            }
+
+            await conn.commit(); // commit transaction
+        } catch (err) {
+            await conn.rollback(); // rollback transaction on error
+            throw err;
+        } finally {
+            conn.release(); // release connection
+        }
+    }
 }
+
